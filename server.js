33219e7d5e150dba9d5cc35c2576871485db4e41
Peter Martinson <peter.j.martinson@gmail.com>
2016-04-08 01:27:25 -0400

    api done

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-
+/*
+ * https://scotch.io/tutorials/creating-a-single-page-todo-app-with-node-and-angular
+*/
 
 // set up ============================
 var express = reqire('express');
@@ -19,6 +21,64 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
 app.use(methodOverride());
 
+// define model ====================
+var Todo = mongoose.model('Todo', { text : String } );
+
+// routes ===========================
+  // api ----------------------------
+  app.get('/api/todos', functin(req, res) {
+    
+    // use mongoose to get all records in the database
+    Todo.find(function(err, todos) {
+      
+      // catch the error
+      if (err)
+        res.send(err)
+
+      res.json(todos); // return all todos in JSON format
+    });
+  });
+
+  // CREATE
+  app.post('/api/todos', function(req,res) {
+    
+    // create a todo
+    Todo.create({
+      text : req.body.text,
+      done : false
+    }, function(err, todo) {
+      if (err)
+        res.send(err);
+
+      // get and return all the todos after you create one
+      Todo.find(function(err, todos) {
+        if (err)
+          res.send(err)
+        res.json(todos);
+      });
+    });
+
+  });
+
+  // DELETE
+  app.delete('/api/todos/:todo_id', function(req, res) {
+    Todo.remove({
+      _id : req.params.todo_id
+    }, function(err, todo) {
+      if (err)
+        res.send(err);
+
+      // get and return all the todos
+      Todo.find(function(err, todos) {
+        if (err)
+          res.send(err)
+        res.json(todos);
+      });
+    });
+
+  });
 // listen ===========================
-app.listen(8080);
-console.log("App listening on port 8080');
+var port = process.env.PORT; 
+app.listen( app.get('port'), function() {
+  console.log('App is listening on port ', app.get('port'));
+});
