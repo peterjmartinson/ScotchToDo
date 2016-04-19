76cdb27d8573a6b4c5279ffc54a8e2f8006f09a9
Peter <peter.j.martinson@gmail.com>
2016-04-19 12:34:07 -0400

    changed core2

--- public/core2.js
+++ public/core2.js
@@ -7,43 +7,42 @@
  
 var scotchTodo = angular.module('scotchTodo', []);
 
-//scotchTodo.controller('mainController', ['$scope', '$http',
-//function ($scope, $http) {
-function mainController($scope, $http) {
-  $scope.formData = {};
+scotchTodo.controller('mainController', ['$scope', '$http',
+  function ($scope, $http) {
+    $scope.formData = {};
 
-  // when landing on a page, get all todos and show them
-  $http.get('/api/todos')
-	  .then(function successCallback(res) {
-				$scope.todos = res.data;
-				console.log(res.data);
-			}, function errorCallback(res) {
-				console.log('Error: ' + res.data);
-			});
+    // when landing on a page, get all todos and show them
+    $http.get('/api/todos')
+      .then(function successCallback(res) {
+          $scope.todos = res.data;
+          console.log(res.data);
+        }, function errorCallback(res) {
+          console.log('Error: ' + res.data);
+        });
 
-  // when subitting the add form, send the text to the node API
-  $scope.createTodo = function() {
-    $http.post('/api/todos', $scope.formData)
-		  .then(function successCallback(res) {
-        $scope.formData = {};
-        $scope.todos = res.data;
-        console.log(res.data);
-      }, function errorCallback(res) {
-        console.log('Error: ' + res.data);
-      });
-  };
+    // when subitting the add form, send the text to the node API
+    $scope.createTodo = function() {
+      $http.post('/api/todos', $scope.formData)
+        .then(function successCallback(res) {
+          $scope.formData = {};
+          $scope.todos = res.data;
+          console.log(res.data);
+        }, function errorCallback(res) {
+          console.log('Error: ' + res.data);
+        });
+    };
 
-  // delete a todo after checking it
-  $scope.deleteTodo = function(id) {
-    $http.delete('/api/todos/' + id)
-		  .then(function successCallback(res) {
-        $scope.todos = res.data;
-        console.log(res.data);
-      }, function errorCallback(res) {
-        console.log('Error: ' + res.data);
-      });
-  };
+    // delete a todo after checking it
+    $scope.deleteTodo = function(id) {
+      $http.delete('/api/todos/' + id)
+        .then(function successCallback(res) {
+          $scope.todos = res.data;
+          console.log(res.data);
+        }, function errorCallback(res) {
+          console.log('Error: ' + res.data);
+        });
+    };
 
-}
+});
 
 scotchTodo.controller('mainController', mainController);
